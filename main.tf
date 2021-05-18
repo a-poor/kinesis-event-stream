e6da56459735cd6a5f728e891f41ca3dcb0cf7cc
a-poor <45295232+a-poor@users.noreply.github.com>
2021-05-17 20:47:18 -0700

    Updating `main.tf` -- Fixing IAM permission issue

--- main.tf
+++ main.tf
@@ -11,13 +11,23 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+variable "firehose_stream_name" {
+  type = string
+  default = "kinesis-test-stream"
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = "test-event-stream-bucket"
   acl    = "private"
+  force_destroy = true
 }
 
 resource "aws_kinesis_firehose_delivery_stream" "kinesis_event_stream" {
-  name        = "kinesis-test-stream"
+  name        = var.firehose_stream_name
   destination = "extended_s3"
 
   extended_s3_configuration {
@@ -25,6 +35,7 @@ resource "aws_kinesis_firehose_delivery_stream" "kinesis_event_stream" {
     bucket_arn = aws_s3_bucket.bucket.arn
     buffer_size = 1
     buffer_interval = 60
+    compression_format = "GZIP"
   }
 }
 
@@ -33,8 +44,9 @@ resource "aws_iam_role" "firehose_role" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
+  "Version": "2012-10-17",  
+  "Statement":
+  [
     {
       "Action": "sts:AssumeRole",
       "Principal": {
@@ -46,5 +58,39 @@ resource "aws_iam_role" "firehose_role" {
   ]
 }
 EOF
+
+  inline_policy {
+    name = "kinesis-s3-inline-policy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {      
+          Effect = "Allow",      
+          Action = [
+            "s3:AbortMultipartUpload",
+            "s3:GetBucketLocation",
+            "s3:GetObject",
+            "s3:ListBucket",
+            "s3:ListBucketMultipartUploads",
+            "s3:PutObject"
+          ]      
+          Resource = [        
+            "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}",
+            "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}/*"		    
+          ]    
+        },
+        {
+          Effect = "Allow"
+          Action = [
+            "kinesis:DescribeStream",
+            "kinesis:GetShardIterator",
+            "kinesis:GetRecords",
+            "kinesis:ListShards"
+          ]
+          Resource = "arn:aws:kinesis:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.firehose_stream_name}"
+        }
+      ]
+    })
+  }
 }
 
